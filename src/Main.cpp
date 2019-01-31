af8c3ba1884a7c876c11166a55064f25cd43fbfe
Tom Reed <t.reed1104@gmail.com>
2019-01-31 00:07:48 +0000

    Implemented a test call for LuaScript to make sure it is working as intended

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,10 @@
-#include "LuaScript.h"
+#include "LuaScript.hpp"
 
 int main() {
+	LuaScript script = LuaScript("content/engine_config.lua");
+	
+	int tileSizeX = script.Get<int>("config.window.tile_size.x");
+
 
 	return 0;
 }
